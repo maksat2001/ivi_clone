1be63cb00ec9d61ff619a0cec45eef5610b2d92d
maksat111 <maksatakmyradow111@gmail.com>
2023-02-20 23:43:55 +0500

    change

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Progress } from 'antd';
 import { Link } from 'react-router-dom';
 import cardImage from '../images/cardImg.jfif';
 import './Card.css';
+import { BookmarkRegular, ProhibitedRegular, StarRegular, WandRegular } from '@fluentui/react-icons';
 
 function Card() {
     return (
@@ -17,7 +19,33 @@ function Card() {
                     <img src={cardImage} alt='cardImage' />
                 </div>
                 <div className='card_hover_container'>
-                    salam
+                    <div className='hover_container_right'>
+                        <BookmarkRegular className='hover_icon' />
+                        <WandRegular className='hover_icon' />
+                        <StarRegular className='hover_icon' />
+                        <ProhibitedRegular className='hover_icon' />
+                    </div>
+                    <div className='hover_container_left'>
+                        <div className='card_grade_container'>
+                            <p>8,2</p>
+                            <div className='progress_bars_container'>
+                                <Progress size={'small'} strokeWidth={2} width={25} percent={50} showInfo={false} strokeColor={'#ea003d'} />
+                                <Progress size={'small'} strokeWidth={2} width={25} percent={30} showInfo={false} strokeColor={'#ea003d'} />
+                                <Progress size={'small'} strokeWidth={2} width={25} percent={20} showInfo={false} strokeColor={'#ea003d'} />
+                                <Progress size={'small'} strokeWidth={2} width={25} percent={5} showInfo={false} strokeColor={'#ea003d'} />
+                            </div>
+                        </div>
+                        <div className='syuzet'>
+                            <p>сюжет</p>
+                            <Progress size={'small'} strokeWidth={4} percent={50} showInfo={false} strokeColor={'#ea003d'} />
+                        </div>
+                        <p className='card_hover_country'>2019, Россия, Приключения Приключения</p>
+                        <p className='card_hover_duration'>85 минут</p>
+                    </div>
+
+                    <div className='hover_container_right'>
+
+                    </div>
                 </div>
             </div>
             <div className='card_name_container'>
