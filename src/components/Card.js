0eded82d4cff19508ba85082a9b485fb58834d02
maksat111 <maksatakmyradow111@gmail.com>
2023-02-20 18:53:37 +0500

    change

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,17 +7,22 @@ function Card() {
     return (
         <Link className='card_container'>
             <div className='card_image_container'>
-                <div className='card_badge'>
+                <div className='card_badge card_badge_ivi'>
                     выбор Иви
                 </div>
+                {/* <div className='card_badge card_badge_exclusive'>
+                    эксклюзив
+                </div> */}
                 <div className='card_image'>
                     <img src={cardImage} alt='cardImage' />
                 </div>
                 <div className='card_hover_container'>
+                    salam
                 </div>
             </div>
             <div className='card_name_container'>
                 <p className='movie_name'>Три богатыря и Наследница престола</p>
+                <p className='movie_padpiska'>Бесплатно</p>
             </div>
         </Link>
     );
--- src/pages/home/components/CardGroup.js
+++ src/pages/home/components/CardGroup.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { ChevronRightRegular } from '@fluentui/react-icons';
 import Card from '../../../components/Card';
 import './CardGroup.css';
 
 function CardGroup() {
     return (
         <div className='card_group_container'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <div className='card_group_name'>
+                <p>Рекомендую посмотреть</p>
+                <ChevronRightRegular style={{ fontSize: '28px', fontWeight: '800' }} />
+            </div>
+            <div className='card_group_card_container'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            {/* <Card /> */}
         </div>
     );
 }
